e14b099f5f9e4cd98218f8a98ff77b1c14a0605a
Coline <53483063+coline-tylec@users.noreply.github.com>
2020-07-09 18:21:15 +0200

    [FEAT]Begin CRUD Quest
    merge on develop

--- app/Http/Services/QuestService.php
+++ app/Http/Services/QuestService.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Services;
 
-use App\models\Tip;
+use App\models\Quest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class TipService extends Service
+class QuestService extends Service
 {
     public function __construct()
     {
@@ -14,8 +14,8 @@ class TipService extends Service
     }
 
     /**
-     * Get all tips
-     * Return empty array if no user are present in database
+     * Get all quests
+     * Return empty array if no quests are present in database
      *
      * @return \Illuminate\Http\JsonResponse
      */
@@ -117,8 +117,8 @@ class TipService extends Service
         $tip = Tip::find($id);
         if ($tip) {
             $validator = Validator::make($request->all(), [
-                'name' => 'min:2|max:45',
-                'desc' => 'min:2|max:120',
+                'name' => 'required|max:45',
+                'desc' => 'required|max:120',
             ]);
             if ($validator->fails()) {
                 return [
