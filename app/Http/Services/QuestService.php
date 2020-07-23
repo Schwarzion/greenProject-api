bac7063b0b30907745d045c34e00781c1b49439f
Thomas <th.vionnet@gmail.com>
2020-07-23 12:25:29 +0200

    edit cors path + corrected QuestService update method

--- app/Http/Services/QuestService.php
+++ app/Http/Services/QuestService.php
@@ -122,12 +122,12 @@ class QuestService extends Service
         $quest = Quest::find($id);
         if ($quest) {
             $validator = Validator::make($request->all(), [
-                'name' => 'required|max:45',
-                'desc' => 'required|max:120',
-                'expAmount' => 'required|max:11|integer',
-                'minLevel' => 'required|max:11|integer',
+                'name' => 'max:45',
+                'desc' => 'max:120',
+                'expAmount' => 'max:11|integer',
+                'minLevel' => 'max:11|integer',
                 'timeForQuest' => '',
-                'endDate' => 'required|date'
+                'endDate' => 'date'
             ]);
             if ($validator->fails()) {
                 return [
