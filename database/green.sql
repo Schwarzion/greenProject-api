ce748fd1f45a093c18ee0635e55dc9cea9835a5b
= <=>
2020-10-08 14:12:57 +0200

    SQL last version

--- database/green.sql
+++ database/green.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.17  Distrib 10.3.23-MariaDB, for debian-linux-gnueabihf (armv7l)
 --
 -- Host: localhost    Database: greenDB
 -- ------------------------------------------------------
--- Server version	10.4.13-MariaDB
+-- Server version	10.3.23-MariaDB-0+deb10u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,13 +15,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `level`
---
+
 DROP DATABASE IF EXISTS greenDB;
 CREATE DATABASE greenDB;
 
-USE greenDB;
+--
+-- Table structure for table `level`
+--
 
 DROP TABLE IF EXISTS `level`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -32,7 +32,7 @@ CREATE TABLE `level` (
   `desc` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `levelExpAmount` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +41,7 @@ CREATE TABLE `level` (
 
 LOCK TABLES `level` WRITE;
 /*!40000 ALTER TABLE `level` DISABLE KEYS */;
+INSERT INTO `level` VALUES (1,'Debutant1','Débutant niveau 1',30),(2,'Debutant2','Débutant niveau 2',60),(3,'Debutant3','Débutant niveau 3',90),(4,'Debutant4','Débutant niveau 4',120),(5,'Debutant5','Débutant niveau 5',150),(6,'Intermediaire1','Intermédiaire niveau 1',200),(7,'Intermediaire2','Intermédiaire niveau 2',250),(8,'Intermediaire3','Intermédiaire niveau 3',300),(9,'Intermediaire4','Intermédiaire niveau 4',350),(10,'Intermediaire5','Intermédiaire niveau 5',400),(11,'Avance1','Avancé niveau 1',470),(12,'Avance2','Avancé niveau 2',540),(13,'Avance3','Avancé niveau 3',610),(14,'Avance4','Avancé niveau 4',680),(15,'Avance5','Avancé niveau 5',750),(16,'Confirme1','Confirmé niveau 1',840),(17,'Confirme2','Confirmé niveau 2',930),(18,'Confirme3','Confirmé niveau 3',1020),(19,'Confirme4','Confirmé niveau 4',1110),(20,'Confirme5','Confirmé niveau 5',1200),(21,'Expert1','Expert niveau 1',1310),(22,'Expert2','Expert niveau 2',1420),(23,'Expert3','Expert niveau 3',1530),(24,'Expert4','Expert niveau 4',1640),(25,'Expert5','Expert niveau 5',1750);
 /*!40000 ALTER TABLE `level` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,7 +113,7 @@ CREATE TABLE `role` (
   `desc` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,6 +122,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'admin','Admin privileges');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,8 +139,8 @@ CREATE TABLE `roleUser` (
   PRIMARY KEY (`userId`,`roleId`),
   KEY `fkIdx_userId` (`userId`),
   KEY `fkIdx_role_id` (`roleId`),
-  CONSTRAINT `FK_role_user` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_user_role` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `FK_role_user` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_user_role` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -148,6 +150,7 @@ CREATE TABLE `roleUser` (
 
 LOCK TABLES `roleUser` WRITE;
 /*!40000 ALTER TABLE `roleUser` DISABLE KEYS */;
+INSERT INTO `roleUser` VALUES (1,1);
 /*!40000 ALTER TABLE `roleUser` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +168,7 @@ CREATE TABLE `tip` (
   `tipStatus` smallint(6) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +177,7 @@ CREATE TABLE `tip` (
 
 LOCK TABLES `tip` WRITE;
 /*!40000 ALTER TABLE `tip` DISABLE KEYS */;
-INSERT INTO `tip` VALUES (1,'testTip','Adieu les bouteilles d''eau, le charbon actif permet d''assainir l''eau du robinet!',1),(2,'coffeeMaker','Si tu souhaites boire ton café sans déchet, rien de mieux qu''une cafetière italienne, à piston ou un filtre lavable!',1),(3,'shower','Remplace ton shampoing et ton gel douche par des solutions solides afin d''éviter les emballages!',1),(4,'stopPub','N''hésites pas à mettre un autocollant "Stop Pub" sur ta boite aux lettres si tu es contre la production intensive de papier et sauve des forêts!',1),(5,'fruitsMûrs','Si tu as des fruits trop mûrs, fait des compotes ou des confitures!',1);
+INSERT INTO `tip` VALUES (1,'testTip','Adieu les bouteilles d\'eau, le charbon actif permet d\'assainir l\'eau du robinet!',1),(2,'coffeeMaker','Si tu souhaites boire ton café sans déchet, rien de mieux qu\'une cafetière italienne, à piston ou un filtre lavable!',1),(3,'shower','Remplace ton shampoing et ton gel douche par des solutions solides afin d\'éviter les emballages!',1),(4,'stopPub','N\'hésites pas à mettre un autocollant \"Stop Pub\" sur ta boite aux lettres si tu es contre la production intensive de pap',1),(5,'fruitsMûrs','Si tu as des fruits trop mûrs, fait des compotes ou des confitures!',1);
 /*!40000 ALTER TABLE `tip` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -205,7 +208,7 @@ CREATE TABLE `user` (
   `temparyPasswordValid` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_user` (`email`,`alias`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -214,7 +217,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Joe','Mama','test@mail.com',1,1,'420 Marble Street','Lille','59000','test@mail.com',1,'0752627592','1995-11-20',0,NULL,'$2y$10$ogL1nHZhVaRXpu1Z.vG3DOyT8iZyvnIN.WmDF9pUYd6xvrfOITowq',NULL),(12,'testName','testLastName','testmail@test.fr',1,1,'42 Rue du Test','testCity','75000','testmail@test.fr',1,'0123456789','2000-01-01',0,NULL,'$2y$10$8K976/XC.tSjmKKOTRD/9e1bgsYf0M/wkwRq0VLgaptNQcSw29fF2',NULL),(13,'Joe','Mama','testmai2l@test.fr',1,0,'420 Marble Street','Lille','59000','testmai2l@test.fr',1,'0752627592','1995-11-20',0,NULL,'$2y$10$7tAqL3luTFVVdxIpuxE0ru.MN6mo49qsawClI08xSzSZGZNCTwJEW',NULL);
+INSERT INTO `user` VALUES (1,'Joe','Mama','test@mail.com',1,1,'420 Marble Street','Lille','59000','test@mail.com',1,'0752627592','1995-11-20',0,NULL,'$2y$10$ogL1nHZhVaRXpu1Z.vG3DOyT8iZyvnIN.WmDF9pUYd6xvrfOITowq',NULL),(12,'testName','testLastName','testmail@test.fr',1,1,'42 Rue du Test','testCity','75000','testmail@test.fr',1,'0123456789','2000-01-01',0,NULL,'$2y$10$8K976/XC.tSjmKKOTRD/9e1bgsYf0M/wkwRq0VLgaptNQcSw29fF2',NULL),(13,'Joe','Mama','testmai2l@test.fr',1,0,'420 Marble Street','Lille','59000','testmai2l@test.fr',1,'0752627592','1995-11-20',0,NULL,'$2y$10$7tAqL3luTFVVdxIpuxE0ru.MN6mo49qsawClI08xSzSZGZNCTwJEW',NULL),(14,'toto','vivi','tovi',1,1,'1 rue des papillons','Kiwi','80000','tototest@mail.com',0,'0192837465','1998-01-01',0,NULL,'$2y$10$vFyxaHq/zRLcBU7f8nqBQerW.GyryVQ/teDRtVD7J0f6K8YP/ab2y',NULL),(15,'Joe','Mama','test',1,1,'420 Marble Street','Lille','59000','jean@mail.com',1,'0752627592','1995-11-20',0,NULL,'$2y$10$SwRcSztYJb8647ZfPcLM0eX4RDXkP19sN5.GEJlzczWdp1gVXkGo2',NULL),(16,'Joe','Mama','testhg',1,1,'420 Marble Street','Lille','59000','jean@mail.comht',1,'0752627592','1995-11-20',0,NULL,'$2y$10$RNx5zPexO0YzjZu7bEkYs.Jzucxp0/oBoqHymmEJmZRG3Kn/nSuO.',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,8 +236,8 @@ CREATE TABLE `userQuest` (
   PRIMARY KEY (`questId`,`userId`),
   KEY `fkIdx_questId` (`questId`),
   KEY `fkIdx_userId` (`userId`),
-  CONSTRAINT `FK_quest_user` FOREIGN KEY (`questId`) REFERENCES `quest` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_user_quest` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `FK_quest_user` FOREIGN KEY (`questId`) REFERENCES `quest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_user_quest` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -257,4 +260,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-10 19:48:44
+-- Dump completed on 2020-10-08 14:06:56
